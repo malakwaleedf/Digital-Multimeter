1ef2e48562151c04bfc16b5e644742b286245b8c
Malak Waleed <130199370+malakwaleedf@users.noreply.github.com>
2025-01-30 03:18:07 +0200

    Add files via upload

---
+++ DMM_CODE/DMM/Debug/main1.d
@@ -0,0 +1,14 @@
+main1.o main1.o: ../main1.c ../adc.h ../std_types.h ../gpio.h ../lcd.h \
+  ../keypad.h ../common_macros.h
+
+../adc.h:
+
+../std_types.h:
+
+../gpio.h:
+
+../lcd.h:
+
+../keypad.h:
+
+../common_macros.h:
